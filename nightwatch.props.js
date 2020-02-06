37b1b4fc5d2503e7b8fb426366e1b0d09318c06d
lyndsijae93 <57276055+lyndsijae93@users.noreply.github.com>
2020-02-06 07:42:41 -0700

    Add files via upload

---
+++ nightwatch.props.js
@@ -0,0 +1,5 @@
+module.exports = {
+    resourcePath: "C:\\Users\\LyndsiLittlefield\\Documents\\Anglepoint\\testing-resources\\",
+    seleniumServer: "selenium-server-standalone-3.141.59.jar",
+    chromedriver: "chromedriver.exe"
+ }
